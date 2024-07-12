acef800fa0105d3747051756afd19e9fc88edbd6
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-07-12 23:34:22 +0600

    update

---
+++ JS/ProblemSolve/MoreProblem.js
@@ -0,0 +1,10 @@
+
+// for(let i = 0; i<2; i++){
+//     setTimeout(()=> console.log(i))
+// }
+// Output : 0,1
+// for(var i = 0; i<2; i++){
+//     setTimeout(()=> console.log(i))
+// }
+// output 2,2
+
